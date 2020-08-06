48b290374e9b4cc2932dda0d179113222d8eb801
anushka-srivastava22 <67271376+anushka-srivastava22@users.noreply.github.com>
2020-08-06 14:19:25 +0530

    Add files via upload

---
+++ P2.java
@@ -0,0 +1,16 @@
+class P2
+{
+    public static void main(String args[])
+    {
+        int n=1;
+        for(int x=1;x<=5 ;x++)
+        {
+            for(int y=1;y<=x;y++)
+            {
+                System.out.print(n+++ " ");
+            }
+            System.out.println();
+        }
+    }
+}
+ 
